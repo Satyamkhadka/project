b890c59a606fa3fed64fb6cb8593f9babf4c735e
Satyam Khadka <Satyam.khadka123@gmail.com>
2019-11-05 18:08:13 +0545

    Add files via upload

---
+++ about.php
@@ -0,0 +1,21 @@
+<html>
+<div><br/><center><h2><font face="Lucida Handwriting" size="+1" color="#00CCFF">ABOUT US</font></h2></center></div>
+<div style="width:100%;float:left">
+<div>
+  <p><br/>
+  
+	  <div align="center"><img src="usepics/abt3.jpg"/></div>
+	  <div>
+	  <p>
+	  <p</p><br>
+
+<p>lets make shopping easy </p><br>
+
+
+
+
+</p></p>
+	 </div>
+	  </div>
+	  </div>
+	  </html>
